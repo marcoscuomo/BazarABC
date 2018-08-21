87e20ae066ac4bb47dfa103ec3615be1ea0f6409
bytecore@gmail.com <mo10128>
2018-08-21 19:00:03 -0300

    Criando os atributos do cadastroAcvitivy

--- app/src/main/java/br/com/mojumob/bazarabc/CadastroActivity.java
+++ app/src/main/java/br/com/mojumob/bazarabc/CadastroActivity.java
@@ -2,12 +2,21 @@ package br.com.mojumob.bazarabc;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class CadastroActivity extends AppCompatActivity {
+
+    //Atributos
+    private EditText edtNome, edtEmail, edtSenha, edtSenhaRepetida;
+    private Button btnCadastrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro);
+
+
+
     }
 }
