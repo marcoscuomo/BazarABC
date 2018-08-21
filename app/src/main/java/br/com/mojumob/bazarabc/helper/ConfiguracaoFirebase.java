b9df23fba25dce5bd9a8f155a1f03256ff62712a
bytecore@gmail.com <mo10128>
2018-08-21 19:52:36 -0300

    Criando o metodo de criar cadastro

---
+++ app/src/main/java/br/com/mojumob/bazarabc/helper/ConfiguracaoFirebase.java
@@ -0,0 +1,44 @@
+package br.com.mojumob.bazarabc.helper;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+public class ConfiguracaoFirebase {
+
+    private static DatabaseReference referenciaFirebase;
+    private static FirebaseAuth referenciaAutenticacao;
+    private static StorageReference referenciaStorage;
+
+
+    //retorna a referencia do databse
+    public static DatabaseReference getFirebase(){
+        if(referenciaFirebase == null){
+            referenciaFirebase = FirebaseDatabase.getInstance().getReference();
+        }
+
+        return referenciaFirebase;
+    }
+
+    //retorna a instancia do FirebaseAuth
+    public static FirebaseAuth getFirebaseAutenticacao(){
+        if(referenciaAutenticacao == null){
+            referenciaAutenticacao = FirebaseAuth.getInstance();
+        }
+        return referenciaAutenticacao;
+    }
+
+    //Retorna a referencia do FirebaseStorage
+    public static StorageReference getFirebaseStorafe(){
+        if(referenciaStorage == null){
+            referenciaStorage = FirebaseStorage.getInstance().getReference();
+        }
+
+        return referenciaStorage;
+    }
+
+
+
+}
