bc981a32b45c55b4a11aa8403cb54c545a60adce
bytecore@gmail.com <mo10128>
2018-08-22 16:05:57 -0300

    Altera lsita de strings

--- app/src/main/java/br/com/mojumob/bazarabc/activity/CadastroAnuncioActivity.java
+++ app/src/main/java/br/com/mojumob/bazarabc/activity/CadastroAnuncioActivity.java
@@ -69,6 +69,10 @@ public class CadastroAnuncioActivity extends AppCompatActivity implements View.O
 
     private void carregarDadosSpinner() {
 
+        String[] cidades = new String[]{
+
+        };
+
     }
 
     @Override
