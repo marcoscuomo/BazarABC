cc00c9cffcb4354afe88ccf27cfcff389673db33
bytecore@gmail.com <mo10128>
2018-08-22 16:03:33 -0300

    Cria metodo carregaDadosSpinner

--- app/src/main/java/br/com/mojumob/bazarabc/activity/CadastroAnuncioActivity.java
+++ app/src/main/java/br/com/mojumob/bazarabc/activity/CadastroAnuncioActivity.java
@@ -1,9 +1,15 @@
 package br.com.mojumob.bazarabc.activity;
 
 import android.Manifest;
+import android.app.Activity;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.media.MediaDataSource;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -16,12 +22,14 @@ import android.widget.Toast;
 
 import com.blackcat.currencyedittext.CurrencyEditText;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 
 import br.com.mojumob.bazarabc.R;
 import br.com.mojumob.bazarabc.helper.Permissoes;
 
-public class CadastroAnuncioActivity extends AppCompatActivity {
+public class CadastroAnuncioActivity extends AppCompatActivity implements View.OnClickListener {
 
     //Atributos
     private EditText edtTitulo, edtValor, edtDescricao;
@@ -34,6 +42,7 @@ public class CadastroAnuncioActivity extends AppCompatActivity {
             Manifest.permission.READ_EXTERNAL_STORAGE
     };
 
+    private List<String> listaFotosRecuperada = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,6 +53,7 @@ public class CadastroAnuncioActivity extends AppCompatActivity {
         Permissoes.validarPermissoes(permissoes, this, 1);
 
         inicializaComponentes();
+        carregarDadosSpinner();
 
 
         //Evento clique do botao cadastrar anuncio
@@ -54,6 +64,59 @@ public class CadastroAnuncioActivity extends AppCompatActivity {
             }
         });
 
+
+    }
+
+    private void carregarDadosSpinner() {
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch(v.getId()){
+            case R.id.cadastroAnuncio_imgAnuncio1:
+                escolherImagem(1);
+                break;
+            case R.id.cadastroAnuncio_imgAnuncio2:
+                escolherImagem(2);
+                break;
+            case R.id.cadastroAnuncio_imgAnuncio3:
+                escolherImagem(3);
+                break;
+        }
+
+    }
+
+    private void escolherImagem(int requestCode) {
+        Intent i = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(i, requestCode);
+    }
+
+    //Pega o resultado da Activity
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(resultCode == Activity.RESULT_OK){
+
+            //Recuperar imagem
+            Uri imagemSelecionada = data.getData();
+            String caminhoImagem = imagemSelecionada.toString();
+
+            //Configura a imagem no imageview
+            if(requestCode == 1){
+                imgAnuncio1.setImageURI(imagemSelecionada);
+            }else if(requestCode == 2){
+                imgAnuncio2.setImageURI(imagemSelecionada);
+            }else if(requestCode == 3) {
+                imgAnuncio3.setImageURI(imagemSelecionada);
+            }
+
+            listaFotosRecuperada.add(caminhoImagem);
+        }
 
     }
 
@@ -72,6 +135,13 @@ public class CadastroAnuncioActivity extends AppCompatActivity {
         spCidade     = findViewById(R.id.cadastroAnuncio_spCidade);
         spCategoria  = findViewById(R.id.cadastroAnuncio_spCategoria);
         btnCadastrar = findViewById(R.id.cadastroAnuncio_btnCadastrar);
+        imgAnuncio1  = findViewById(R.id.cadastroAnuncio_imgAnuncio1);
+        imgAnuncio2  = findViewById(R.id.cadastroAnuncio_imgAnuncio2);
+        imgAnuncio3  = findViewById(R.id.cadastroAnuncio_imgAnuncio3);
+
+        imgAnuncio1.setOnClickListener(this);
+        imgAnuncio2.setOnClickListener(this);
+        imgAnuncio3.setOnClickListener(this);
 
         //Configura a localidade para pt-br
         Locale locale = new Locale("pt", "BR");
