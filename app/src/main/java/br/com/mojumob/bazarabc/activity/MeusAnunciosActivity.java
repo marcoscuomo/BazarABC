fd6dcee86a1db5065aa47fa0c64137cc12563562
bytecore@gmail.com <mo10128>
2018-08-22 14:11:47 -0300

    Ajusta arquivos para suportar Toolbar

--- app/src/main/java/br/com/mojumob/bazarabc/activity/MeusAnunciosActivity.java
+++ app/src/main/java/br/com/mojumob/bazarabc/activity/MeusAnunciosActivity.java
@@ -4,23 +4,32 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
-
 import br.com.mojumob.bazarabc.R;
 
-public class MeusAnunciosActivity extends Activity {
+
+public class MeusAnunciosActivity extends AppCompatActivity {
 
     //Atributos
     private FloatingActionButton fab;
     private RecyclerView recyclerAnuncios;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_meus_anuncios);
-        getActionBar().setDisplayHomeAsUpEnabled(true);
+
         inicializaComponentes();
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+
 
         //Evento de click no FAB
         fab.setOnClickListener(new View.OnClickListener() {
@@ -35,8 +44,10 @@ public class MeusAnunciosActivity extends Activity {
 
     private void inicializaComponentes() {
         //Inicializações
+        //toolbar = findViewById(R.id.toolbar);
         fab = findViewById(R.id.fabMeusAnuncios);
         recyclerAnuncios = findViewById(R.id.recyclerAnuncios);
+
     }
 
 }
