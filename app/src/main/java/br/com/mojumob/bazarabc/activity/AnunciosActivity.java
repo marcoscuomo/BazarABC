83fb4a575d2752720865e63dacad09ee1080b9ce
bytecore@gmail.com <mo10128>
2018-08-22 10:44:11 -0300

    Sobescreve o metodo onPrepareOptionsMenu

--- app/src/main/java/br/com/mojumob/bazarabc/activity/AnunciosActivity.java
+++ app/src/main/java/br/com/mojumob/bazarabc/activity/AnunciosActivity.java
@@ -2,22 +2,32 @@ package br.com.mojumob.bazarabc.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+
+import com.google.firebase.auth.FirebaseAuth;
+
 import br.com.mojumob.bazarabc.R;
+import br.com.mojumob.bazarabc.helper.ConfiguracaoFirebase;
 
 
 public class AnunciosActivity extends AppCompatActivity {
 
     //Atributos
+    private FirebaseAuth autenticacao;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_anuncios);
+        inicializacoes();
 
 
+    }
+
+    private void inicializacoes() {
+        //Inicializações
+        autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
     }
 
     @Override
@@ -28,6 +38,16 @@ public class AnunciosActivity extends AppCompatActivity {
 
     @Override
     public boolean onPrepareOptionsMenu(Menu menu) {
+
+        //Verifica se o usuario esta logado
+        if(autenticacao == null){
+            //Usuario deslogado
+            menu.setGroupVisible(R.id.group_deslogado, true);
+        }else{
+            //Usuario logado
+            menu.setGroupVisible(R.id.group_logado, true);
+        }
+
         return super.onPrepareOptionsMenu(menu);
     }
 }
