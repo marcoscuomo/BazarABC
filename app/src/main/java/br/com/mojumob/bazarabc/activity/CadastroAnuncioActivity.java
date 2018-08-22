fb6b6bc20226744cd4b3c24be28e53d94498aacb
bytecore@gmail.com <mo10128>
2018-08-22 12:42:59 -0300

    Cria o CadastroAnuncioActivity

---
+++ app/src/main/java/br/com/mojumob/bazarabc/activity/CadastroAnuncioActivity.java
@@ -0,0 +1,15 @@
+package br.com.mojumob.bazarabc.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import br.com.mojumob.bazarabc.R;
+
+public class CadastroAnuncioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro_anuncio);
+    }
+}
