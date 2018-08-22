7f012180d496e51c9dc1717c44bf49991e2b2365
bytecore@gmail.com <mo10128>
2018-08-22 12:40:07 -0300

    Cria e inicializa os compnetes do xml

--- app/src/main/java/br/com/mojumob/bazarabc/activity/MeusAnunciosActivity.java
+++ app/src/main/java/br/com/mojumob/bazarabc/activity/MeusAnunciosActivity.java
@@ -2,16 +2,26 @@ package br.com.mojumob.bazarabc.activity;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.widget.RecyclerView;
 
 import br.com.mojumob.bazarabc.R;
 
 public class MeusAnunciosActivity extends Activity {
+
+    //Atributos
+    private FloatingActionButton fab;
+    private RecyclerView recyclerAnuncios;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_meus_anuncios);
         getActionBar().setDisplayHomeAsUpEnabled(true);
+
+        //Inicializações
+        fab = findViewById(R.id.fabMeusAnuncios);
+        recyclerAnuncios = findViewById(R.id.recyclerAnuncios);
     }
 
 }
