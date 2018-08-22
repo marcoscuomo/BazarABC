c05e42c84f74be553629dea4e1e08e904231ba68
bytecore@gmail.com <mo10128>
2018-08-22 15:42:11 -0300

    Cria o array de permissoes

--- app/src/main/java/br/com/mojumob/bazarabc/activity/CadastroAnuncioActivity.java
+++ app/src/main/java/br/com/mojumob/bazarabc/activity/CadastroAnuncioActivity.java
@@ -1,6 +1,10 @@
 package br.com.mojumob.bazarabc.activity;
 
 import android.Manifest;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -15,6 +19,7 @@ import com.blackcat.currencyedittext.CurrencyEditText;
 import java.util.Locale;
 
 import br.com.mojumob.bazarabc.R;
+import br.com.mojumob.bazarabc.helper.Permissoes;
 
 public class CadastroAnuncioActivity extends AppCompatActivity {
 
@@ -36,7 +41,7 @@ public class CadastroAnuncioActivity extends AppCompatActivity {
         setContentView(R.layout.activity_cadastro_anuncio);
 
         //Validar permissoes
-
+        Permissoes.validarPermissoes(permissoes, this, 1);
 
         inicializaComponentes();
 
@@ -71,5 +76,32 @@ public class CadastroAnuncioActivity extends AppCompatActivity {
         //Configura a localidade para pt-br
         Locale locale = new Locale("pt", "BR");
         campoValor.setLocale(locale);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        for(int permissaoResultado : grantResults){
+            if(permissaoResultado == PackageManager.PERMISSION_DENIED){
+                alertaValidacaoPermissao();
+            }
+        }
+    }
+
+    private void alertaValidacaoPermissao(){
+        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+        alert.setTitle("Permissao negada");
+        alert.setMessage("Para utilizar o app é necessário aceitar as permissões");
+        alert.setCancelable(false);
+        alert.setPositiveButton("Confirmar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                finish();
+            }
+        });
+
+        AlertDialog dialog = alert.create();
+        alert.show();
     }
 }
