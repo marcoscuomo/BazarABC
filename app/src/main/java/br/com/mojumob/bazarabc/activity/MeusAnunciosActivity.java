5dfb112537a13042fbff1c079c8fc036cd0509e7
bytecore@gmail.com <mo10128>
2018-08-22 12:41:57 -0300

    Criar o evento de click no fab

--- app/src/main/java/br/com/mojumob/bazarabc/activity/MeusAnunciosActivity.java
+++ app/src/main/java/br/com/mojumob/bazarabc/activity/MeusAnunciosActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 
 import br.com.mojumob.bazarabc.R;
 
@@ -18,7 +19,20 @@ public class MeusAnunciosActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_meus_anuncios);
         getActionBar().setDisplayHomeAsUpEnabled(true);
+        inicializaComponentes();
 
+        //Evento de click no FAB
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+
+    }
+
+    private void inicializaComponentes() {
         //Inicializações
         fab = findViewById(R.id.fabMeusAnuncios);
         recyclerAnuncios = findViewById(R.id.recyclerAnuncios);
