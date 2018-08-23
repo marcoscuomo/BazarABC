e3f64096678f699e38967baea276dc87306d6cf3
bytecore@gmail.com <mo10128>
2018-08-23 11:26:19 -0300

    Cria DetalhesProdutoActivity

---
+++ app/src/main/java/br/com/mojumob/bazarabc/activity/DetalhesProdutosActivity.java
@@ -0,0 +1,15 @@
+package br.com.mojumob.bazarabc.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import br.com.mojumob.bazarabc.R;
+
+public class DetalhesProdutosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalhes_produtos);
+    }
+}
