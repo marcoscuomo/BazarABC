389ad3b8acef3e5249be810e97978081355118d9
bytecore@gmail.com <mo10128>
2018-08-23 12:02:15 -0300

    Configura a exibição nos campos pelo GetIntent

--- app/src/main/java/br/com/mojumob/bazarabc/activity/DetalhesProdutosActivity.java
+++ app/src/main/java/br/com/mojumob/bazarabc/activity/DetalhesProdutosActivity.java
@@ -3,11 +3,15 @@ package br.com.mojumob.bazarabc.activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.squareup.picasso.Picasso;
 import com.synnapps.carouselview.CarouselView;
+import com.synnapps.carouselview.ImageListener;
 
 import br.com.mojumob.bazarabc.R;
+import br.com.mojumob.bazarabc.model.Anuncio;
 
 public class DetalhesProdutosActivity extends AppCompatActivity {
 
@@ -15,13 +19,40 @@ public class DetalhesProdutosActivity extends AppCompatActivity {
     private CarouselView carouselView;
     private TextView txtTitulo, txtPreco, txtDescricao, txtCidade;
     private Button btnLigar;
-
+    private Anuncio anuncioSelecionado;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detalhes_produtos);
 
+        //Configura a Toolbar
+
+
+        //Inicializa os componentes da interface
         inicializaComponentes();
+
+
+        //Recupera o anuncio para exibição
+        anuncioSelecionado = (Anuncio) getIntent().getSerializableExtra("anuncioSelecionado");
+
+        if(anuncioSelecionado != null){
+            txtTitulo.setText(anuncioSelecionado.getTitulo());
+            txtPreco.setText(anuncioSelecionado.getValor());
+            txtCidade.setText(anuncioSelecionado.getCidade());
+            txtDescricao.setText(anuncioSelecionado.getDescricao());
+
+            //Configurando o Carrosel
+            ImageListener imageListener = new ImageListener() {
+                @Override
+                public void setImageForPosition(int position, ImageView imageView) {
+                    String urlString = anuncioSelecionado.getFotos().get(position);
+                    Picasso.get().load(urlString).into(imageView);
+                }
+            };
+
+            carouselView.setPageCount(anuncioSelecionado.getFotos().size());
+            carouselView.setImageListener(imageListener);
+        }
 
     }
 
