c1d79d9676612f8add594f5e9f2a49599d8e9c34
bytecore@gmail.com <mo10128>
2018-08-22 22:19:10 -0300

    Cria o metodo salvarAnunciosPublicos()

--- app/src/main/java/br/com/mojumob/bazarabc/model/Anuncio.java
+++ app/src/main/java/br/com/mojumob/bazarabc/model/Anuncio.java
@@ -32,6 +32,19 @@ public class Anuncio {
                 .child(idUsuario)
                 .child(getIdAnuncio())
                 .setValue(this);
+
+        salvarAnuncioPublico();
+    }
+
+    public void salvarAnuncioPublico(){
+        DatabaseReference anuncioRef = ConfiguracaoFirebase.getFirebase()
+                .child("anuncios");
+
+        anuncioRef
+                .child(getCidade())
+                .child(getCategoria())
+                .child(getIdAnuncio())
+                .setValue(this);
     }
 
     public String getIdAnuncio() {
