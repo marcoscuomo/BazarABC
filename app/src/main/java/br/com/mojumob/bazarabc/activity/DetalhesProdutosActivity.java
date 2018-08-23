9a9bbb1be4a247eca7b1c4d41cfbb6f2c57b9266
bytecore@gmail.com <mo10128>
2018-08-23 11:51:41 -0300

    Cria e inicializa os componenets

--- app/src/main/java/br/com/mojumob/bazarabc/activity/DetalhesProdutosActivity.java
+++ app/src/main/java/br/com/mojumob/bazarabc/activity/DetalhesProdutosActivity.java
@@ -2,14 +2,34 @@ package br.com.mojumob.bazarabc.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.synnapps.carouselview.CarouselView;
 
 import br.com.mojumob.bazarabc.R;
 
 public class DetalhesProdutosActivity extends AppCompatActivity {
 
+    //Atributos
+    private CarouselView carouselView;
+    private TextView txtTitulo, txtPreco, txtDescricao, txtCidade;
+    private Button btnLigar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detalhes_produtos);
+
+        inicializaComponentes();
+
+    }
+
+    private void inicializaComponentes() {
+        txtTitulo = findViewById(R.id.detalhes_txtTitulo);
+        txtPreco = findViewById(R.id.detalhes_txtPreco);
+        txtCidade = findViewById(R.id.detalhes_txtCidade);
+        txtDescricao = findViewById(R.id.detalhes_txtDescricao);
+        btnLigar = findViewById(R.id.detalhes_btnLigar);
     }
 }
