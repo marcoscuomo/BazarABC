caa49cfe49fad44de0345e128bb78c5879579557
bytecore@gmail.com <mo10128>
2018-08-23 10:07:09 -0300

    Configura o RecyclerView para exibor os anuncios

--- app/src/main/java/br/com/mojumob/bazarabc/activity/AnunciosActivity.java
+++ app/src/main/java/br/com/mojumob/bazarabc/activity/AnunciosActivity.java
@@ -3,18 +3,37 @@ package br.com.mojumob.bazarabc.activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 import br.com.mojumob.bazarabc.R;
+import br.com.mojumob.bazarabc.adapter.AdapterAnuncios;
 import br.com.mojumob.bazarabc.helper.ConfiguracaoFirebase;
+import br.com.mojumob.bazarabc.model.Anuncio;
 
 
 public class AnunciosActivity extends AppCompatActivity {
 
     //Atributos
     private FirebaseAuth autenticacao;
+    private Button btnRegiao, btnCategoria;
+    private RecyclerView recyclerAnunciosPublicos;
+    private List<Anuncio> listaAnuncios = new ArrayList<>();
+    private AdapterAnuncios adapter;
+    private DatabaseReference anunciosPublicosRef;
     //private Toolbar toolbar;
 
 
@@ -22,12 +41,58 @@ public class AnunciosActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_anuncios);
-        inicializacoes();
+        inicializaComponentes();
+
+        //Configurações iniciais
+        autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
+        anunciosPublicosRef = ConfiguracaoFirebase.getFirebase()
+                                                    .child("anuncios");
+
+
+        //Configura RecylerView
+        recyclerAnunciosPublicos.setLayoutManager(new LinearLayoutManager(AnunciosActivity.this));
+        recyclerAnunciosPublicos.setHasFixedSize(true);
+        adapter = new AdapterAnuncios(AnunciosActivity.this, listaAnuncios);
+        recyclerAnunciosPublicos.setAdapter(adapter);
+
+        recuperaAnunciosPublicos();
 
         //Toolbar
         //setSupportActionBar(toolbar);
         //getSupportActionBar().setDisplayShowHomeEnabled(true);
 
+
+    }
+
+    public void recuperaAnunciosPublicos(){
+
+        listaAnuncios.clear();
+        anunciosPublicosRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                for (DataSnapshot cidades :
+                        dataSnapshot.getChildren()) {
+                    for (DataSnapshot categorias :
+                            cidades.getChildren()) {
+                        for (DataSnapshot anunciosDS :
+                                categorias.getChildren()) {
+                            Anuncio anuncio = anunciosDS.getValue(Anuncio.class);
+                            listaAnuncios.add(anuncio);
+                            Collections.reverse(listaAnuncios);
+                            adapter.notifyDataSetChanged();
+                        }
+
+                    }
+                }
+                
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
 
     }
 
@@ -75,9 +140,12 @@ public class AnunciosActivity extends AppCompatActivity {
         startActivity(new Intent(AnunciosActivity.this, LoginActivity.class));
     }
 
-    private void inicializacoes() {
+    private void inicializaComponentes() {
         //Inicializações
         autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
+        btnCategoria = findViewById(R.id.anuncios_btnCategoria);
+        btnRegiao    = findViewById(R.id.anuncios_btnRegia);
+        recyclerAnunciosPublicos = findViewById(R.id.anuncios_recyclerAnunciosPublicos);
         //toolbar = findViewById(R.id.toolbar);
     }
 }
